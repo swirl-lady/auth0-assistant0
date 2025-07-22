d8f1c17605e0788d6275028cf9e3f1b96e4e726f
Deepu K Sasidharan <d4udts@gmail.com>
2025-07-22 12:06:10 +0200

    [core] move vercel ai version to monorepo

---
+++ ts-vercel-ai/src/app/api/chat/route.ts
@@ -0,0 +1,67 @@
+import { NextRequest } from 'next/server';
+import { streamText, Message, createDataStreamResponse, DataStreamWriter } from 'ai';
+import { openai } from '@ai-sdk/openai';
+import { setAIContext } from '@auth0/ai-vercel';
+import { errorSerializer, withInterruptions } from '@auth0/ai-vercel/interrupts';
+
+import { serpApiTool } from '@/lib/tools/serpapi';
+import { getUserInfoTool } from '@/lib/tools/user-info';
+import { gmailDraftTool, gmailSearchTool } from '@/lib/tools/gmail';
+import { checkUsersCalendarTool } from '@/lib/tools/google-calender';
+import { shopOnlineTool } from '@/lib/tools/shop-online';
+import { getContextDocumentsTool } from '@/lib/tools/context-docs';
+const AGENT_SYSTEM_TEMPLATE = `You are a personal assistant named Assistant0. You are a helpful assistant that can answer questions and help with tasks. You have access to a set of tools, use the tools as needed to answer the user's question. Render the email body as a markdown block, do not wrap it in code blocks.`;
+
+/**
+ * This handler initializes and calls an tool calling agent.
+ */
+export async function POST(req: NextRequest) {
+  const request = await req.json();
+
+  const messages = sanitizeMessages(request.messages);
+
+  setAIContext({ threadID: request.id });
+
+  const tools = {
+    serpApiTool,
+    getUserInfoTool,
+    gmailSearchTool,
+    gmailDraftTool,
+    checkUsersCalendarTool,
+    shopOnlineTool,
+    getContextDocumentsTool,
+  };
+
+  return createDataStreamResponse({
+    execute: withInterruptions(
+      async (dataStream: DataStreamWriter) => {
+        const result = streamText({
+          model: openai('gpt-4o-mini'),
+          system: AGENT_SYSTEM_TEMPLATE,
+          messages,
+          maxSteps: 5,
+          tools,
+        });
+
+        result.mergeIntoDataStream(dataStream, {
+          sendReasoning: true,
+        });
+      },
+      {
+        messages,
+        tools,
+      },
+    ),
+    onError: errorSerializer((err: any) => {
+      console.log(err);
+      return `An error occurred! ${err.message}`;
+    }),
+  });
+}
+
+// Vercel AI tends to get stuck when there are incomplete tool calls in messages
+const sanitizeMessages = (messages: Message[]) => {
+  return messages.filter(
+    (message) => !(message.role === 'assistant' && message.parts && message.parts.length > 0 && message.content === ''),
+  );
+};
---
+++ ts-vercel-ai/src/lib/tools/google-calender.ts
@@ -0,0 +1,53 @@
+import { tool } from 'ai';
+import { addHours, formatISO } from 'date-fns';
+import { GaxiosError } from 'gaxios';
+import { google } from 'googleapis';
+import { z } from 'zod';
+import { FederatedConnectionError } from '@auth0/ai/interrupts';
+
+import { getAccessToken, withGoogleConnection } from '../auth0-ai';
+
+export const checkUsersCalendarTool = withGoogleConnection(
+  tool({
+    description: 'Check user availability on a given date time on their calendar',
+    parameters: z.object({
+      date: z.coerce.date(),
+    }),
+    execute: async ({ date }) => {
+      // Get the access token from Auth0 AI
+      const accessToken = await getAccessToken();
+
+      // Google SDK
+      try {
+        const calendar = google.calendar('v3');
+        const auth = new google.auth.OAuth2();
+
+        auth.setCredentials({
+          access_token: accessToken,
+        });
+
+        const response = await calendar.freebusy.query({
+          auth,
+          requestBody: {
+            timeMin: formatISO(date),
+            timeMax: addHours(date, 1).toISOString(),
+            timeZone: 'UTC',
+            items: [{ id: 'primary' }],
+          },
+        });
+
+        return {
+          available: response.data?.calendars?.primary?.busy?.length === 0,
+        };
+      } catch (error) {
+        if (error instanceof GaxiosError) {
+          if (error.status === 401) {
+            throw new FederatedConnectionError(`Authorization required to access the Federated Connection`);
+          }
+        }
+
+        throw error;
+      }
+    },
+  }),
+);
